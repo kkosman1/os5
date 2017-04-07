299b96d1115b304040f0ff2e2817f23a8766b8a5
Sarah Elizabeth Olson <solson3@student02.cse.nd.edu>
2017-04-07 14:42:28 -0400

    added more to tests

--- tests.sh
+++ tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-for FRAME in 25 50 75; do
-	for PROGRAM in "focus" "scan" "sort"; do
-		for ALGORITHM in "custom" "fifo" "rand"; do
-			echo
-			echo $FRAME $ALGORITHM $PROGRAM:
-			./virtmem 100 $FRAME $ALGORITHM $PROGRAM
+for PROGRAM in "focus" "scan" "sort"; do
+	for ALGORITHM in "custom" "fifo" "rand"; do
+		for FRAME in 25 50 75; do
+			echo >> $PROGRAM.txt
+			echo $FRAME $ALGORITHM $PROGRAM: >> $PROGRAM.txt
+			./virtmem 100 $FRAME $ALGORITHM $PROGRAM >> $PROGRAM.txt 
 		done
 	done
 done
